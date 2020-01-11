20ec777230f416c1220e12e3c03643b7dc5044f0
Scott Wilson <scottwilson1020@gmail.com>
2020-01-11 04:34:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,13 +4,14 @@
 
 
 def fizzbuzz(int)
-  if int % 3 == 0 
-    "Fizz"
+  if  int % 3 == 0 && int % 5 == 0
+    "Fizzbuzz"
+    
   else if int % 5 == 0  
     "Buzz"
   
-  else if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
+  else if
+    "FizzBuzz" int % 3 == 0 
     
       end
     end
